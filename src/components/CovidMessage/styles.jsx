4014c5e9e075561a8ad49f2e987c86165bf3ab93
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-03-09 17:37:19 -0500

    added base maps and Type rows. started with google autofill and places intigration

---
+++ src/components/CovidMessage/styles.jsx
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2b80ff',
+    padding: 15,
+    boarderTopLeftRadius: 10,
+    boarderTopRightRadius: 10,
+  },
+  title: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    color: '#bed9ff',
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  learnMore: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
